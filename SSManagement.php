ef90ff05690193d83d33a58eba552cd1fc5d1e67
TungstenVn <40385888+TungstenVn@users.noreply.github.com>
2019-06-16 11:09:06 +0700

    Add files via upload

---
+++ SSManagement.php
@@ -0,0 +1,11 @@
+<?php
+namespace tungsten_simonsay;
+class SSManagement{
+    /** @var array */
+    public static $games = []; //Game in play
+    /** @var array */
+    public static $players = []; //Name
+		
+	public static $number = []; //Number of player in a game
+	public static $blockPlace = []; //Block place, to reset map
+}
